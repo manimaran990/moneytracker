0f6e4c564724c158476425b31f6c9b9c2bbfd552
manimaran990 <manimaran990@gmail.com>
2022-08-07 22:08:04 -0400

    MTBRANCH001: mongodb and retrive data servcies

--- models.py
+++ models.py
@@ -1,35 +1,43 @@
 from pydantic import BaseModel
 from enum import Enum
-from datetime import datetime
-
+import datetime
 
 class ExpenseCats(str, Enum):
     """expense category enum"""
-    shopping     : 'shopping'
-    hotel        : 'hotel'
-    food         : 'food'
-    entertainment: 'entertainment'
-    houserent    : 'houserent'
-    phonebill    : 'phonebill'
-    misc         : 'misc'
+    shopping = 'shopping'
+    hotel = 'hotel'
+    food = 'food'
+    entertainment = 'entertainment'
+    houserent = 'houserent'
+    phonebill = 'phonebill'
+    misc = 'misc'
+    transfers = 'transfers'
+    paycheck = 'paycheck'
+    remittance = 'remittance'
+    service = 'service'
+    bill_payment = 'bill_payment'
+
 
 class CreditOrDebit(str, Enum):
-    credit : 'credit'
-    debit  : 'debit'
+    credit = 'credit'
+    debit = 'debit'
+
 
 class CurrencyType(str, Enum):
-    CAD    : 'CAD'
-    INR    : 'INR'
+    CAD = 'CAD'
+    INR = 'INR'
+
 
 class Expense(BaseModel):
     """single expense model"""
-    user            : str = None
-    date            : datetime = None
-    expense_type    : ExpenseCats = None
-    credit_or_debit : CreditOrDebit = None
-    amount          : float = None
-    currency_type   : CurrencyType = None
-    description     : str = None
+    user: str = None
+    date: datetime.date = None
+    expense_type: ExpenseCats = None
+    credit_or_debit: CreditOrDebit = 'debit'
+    amount: float = None
+    currency_type: CurrencyType = None
+    description: str = None
+
 
 class Item(BaseModel):
-    name: str
+    name = str
